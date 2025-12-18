61bc3b439d14b874d03444a0af22ec1782abcfb2
Rafael Souto <contato@rafaelxsouto.com.br>
2025-12-18 00:14:24 -0300

    feat: enhance error handling with structured error responses

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers
@@ -19,12 +20,17 @@ namespace CashFlow.Api.Controllers
             }
             catch (ArgumentException ex)
             {
-                return BadRequest(ex.Message);
+                var errorResponse = new ResponseErrorJson(ex.Message);
+                
+                return BadRequest(errorResponse);
             }
             catch
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "unknown error");
-                ;
+                var errorResponse = new ResponseErrorJson("unknown error");
+               
+                
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+                
             }
             
             
