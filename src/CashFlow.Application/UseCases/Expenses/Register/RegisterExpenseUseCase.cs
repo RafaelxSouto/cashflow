d24929bf5756b644c11db9bf53d115c528029b84
Rafael Souto <contato@rafaelxsouto.com.br>
2025-12-17 22:19:19 -0300

    feat: add validation for expense registration request

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,3 +1,4 @@
+using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
@@ -7,6 +8,34 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
+        Validade(request);
+        
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validade(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required.");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero.");
+        }
+        
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future.");
+        }
+
+        var paymentsTypeIsValid = Enum.IsDefined(typeof(PaymentsType), request.PaymentsType);
+        if (paymentsTypeIsValid)
+        {
+            throw new ArgumentException("Payments Type is invalid.");
+        }
     }
 }
