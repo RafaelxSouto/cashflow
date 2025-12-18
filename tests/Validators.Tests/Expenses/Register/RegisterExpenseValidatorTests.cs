141815cfebbc4064938f976ce2c8c35973a67f68
Rafael Souto <contato@rafaelxsouto.com.br>
2025-12-18 14:36:43 -0300

    feat: implement Expense entity and PaymentType enum with validation tests

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,6 +1,8 @@
+using System.Data;
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 using Xunit;
@@ -22,5 +24,39 @@ public class RegisterExpenseValidatorTests
 
         //Assert -> COMPARAÇÃO DO TESTE
         result.IsValid.Should().BeTrue();
+    }
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        //Arrange -> PREPARAÇÃO DO TESTE
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJsonBuilder().Build();
+        request.Title = string.Empty;
+        
+
+        //Act -> AÇÃO DE TESTE
+        var result = validator.Validate(request);
+
+        //Assert -> COMPARAÇÃO DO TESTE
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle(e => e.ErrorMessage.Equals("blablabla")).And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+    }
+    
+    [Fact]
+    public void Error_Date_Future()
+    {
+        //Arrange -> PREPARAÇÃO DO TESTE
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJsonBuilder().Build();
+        request.Date = DateTime.UtcNow.AddDays(-1);
+        
+
+        //Act -> AÇÃO DE TESTE
+        var result = validator.Validate(request);
+
+        //Assert -> COMPARAÇÃO DO TESTE
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle(e => e.ErrorMessage.Equals(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE));
     }
 }
